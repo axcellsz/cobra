e8a6d2f18b58b55ab4ca1dcccbeab73e12c3ce7e
axcellsz <122135545+axcellsz@users.noreply.github.com>
2025-11-28 14:32:18 +0700

    Delete worker.js

--- worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    return new Response("WOLLEZ", {
-      headers: {
-        "content-type": "text/plain; charset=utf-8",
-      },
-    });
-  },
-};
