cb0559545e6e41f9a9b3cbeafd75f21cf3a44ab7
axcellsz <122135545+axcellsz@users.noreply.github.com>
2025-11-28 12:17:29 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,7 @@
 
+const worker = new Worker("worker.js");
+
+worker.onmessage = (event) => {
+    console.log(event.data); // Akan menampilkan: WOLLEZ
+    document.body.innerText = event.data; // Menampilkan di halaman
+};
